10dedd2c883463a64cfaaf20fdadaa56a1a454b8
ThibaultNG <byosoris@gmail.com>
2023-06-21 16:45:37 +0200

    eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,15 @@ module.exports = {
 	root: true,
 	extends: ["eslint:recommended"],
 	parserOptions: {
-		ecmaVersion: "latest"
+		ecmaVersion: "latest",
+		sourceType: "module",
+		parser: "@typescript-eslint/parser"
+	},
+	plugins: ["@typescript-eslint"],
+	parser: "@typescript-eslint/parser",
+	env: {
+		browser: true,
+		node: true,
+		es6: true
 	}
 };
