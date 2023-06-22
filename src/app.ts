e9bfe5cb91d4573fa386326c3e400a551c348af0
CamilleDU3 <camille3du@gmail.com>
2023-06-22 11:34:44 +0200

    printing ip and port instead of just port when running main()

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express, { Application } from "express";
 import addRoutes from "api/routes/appRoutes.ts";
 
 const app: Application = express();
+const hostname: string = "localhost";
 const port: number = 5000;
 
 addRoutes(app);
 
-app.listen(port, () => {
-	console.log("App is listening on port " + port);
+app.listen(port, hostname, () => {
+	console.log("running on = " + hostname + ":" + port);
 });
 
 app.use(express.json);
