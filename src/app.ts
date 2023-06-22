705425b22a85937fd8ac8df85867bf005da06633
ThibaultNG <byosoris@gmail.com>
2023-06-22 14:16:09 +0200

    allow CORS on all origins

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from "express";
 import addRoutes from "@/api/routes/addRoutes";
+import cors from "cors";
 
 const app: Application = express();
 const hostname: string = "127.0.0.1";
 const port: number = 5000;
 
+app.use(cors())
 addRoutes(app);
 
 app.listen(port, hostname, () => {
