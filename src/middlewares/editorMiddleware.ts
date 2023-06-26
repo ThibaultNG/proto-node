c7792343feb3a0a8bb4d0f48b43b85582d266ea1
ThibaultNG <byosoris@gmail.com>
2023-06-26 14:39:42 +0200

    save file with name

--- src/middlewares/editorMiddleware.ts
+++ src/middlewares/editorMiddleware.ts
@@ -4,7 +4,10 @@ import fs from "fs";
 export function getFile(req: Request, res: Response): void {
 	if (req.query.fileName) {
 		try {
-			const file = fs.readFileSync(process.env.DEMBOOST + "/config/"+req.query.fileName, "utf8");
+			const file = fs.readFileSync(
+				process.env.DEMBOOST + "/config/" + req.query.fileName,
+				"utf8"
+			);
 			res.status(200).send(file);
 		} catch (err) {
 			res.status(500).send("Error : could not fetch file with name " + req.query.fileName);
@@ -15,15 +18,20 @@ export function getFile(req: Request, res: Response): void {
 }
 
 export function saveFile(req: Request, res: Response): void {
-	if (req.body.data) {
+	if (req.body.fileName && req.body.content) {
+		console.log(req.body);
+
 		try {
-			fs.writeFileSync(process.env.DEMBOOST + "/config/config_temp.yaml", req.body.data);
+			fs.writeFileSync(
+				process.env.DEMBOOST + "/config/" + req.body.fileName,
+				req.body.content
+			);
 			res.status(200).send("File successfully saved");
 		} catch (err) {
 			res.status(500).send("Could not save file");
 		}
 	} else {
-		res.status(400).send("Missing data");
+		res.status(400).send("Missing fileName and/or content");
 	}
 }
 export function getFileList(req: Request, res: Response) {
