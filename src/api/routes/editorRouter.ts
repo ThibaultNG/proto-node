93a5dc258a5647b09b85d102ae93112d4f6c20bf
ThibaultNG <byosoris@gmail.com>
2023-06-26 12:04:31 +0200

    post api to save config file

--- src/api/routes/editorRouter.ts
+++ src/api/routes/editorRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import * as editorMiddleware from "@/middlewares/editorMiddleware";
+import express from "express";
 
 const editorRouter: Router = Router();
 
 editorRouter.get("/config", editorMiddleware.getFile);
+editorRouter.post("/config", express.json(), editorMiddleware.saveFile);
 
 export default editorRouter;
--- src/middlewares/editorMiddleware.ts
+++ src/middlewares/editorMiddleware.ts
@@ -9,3 +9,16 @@ export function getFile(req: Request, res: Response): void {
 		console.error(err);
 	}
 }
+
+export function saveFile(req: Request, res: Response): void {
+	if (req.body.data) {
+		try {
+			fs.writeFileSync(process.env.DEMBOOST + "/config/config_temp.yaml", req.body.data);
+			res.status(200).send("File successfully saved");
+		} catch (err) {
+			res.status(500).send("Could not save file");
+		}
+	} else {
+		res.status(400).send("Missing data");
+	}
+}
