febdb74661e12403ea0f1f68cc0a143cd98e30b6
CamilleDU3 <camille3du@gmail.com>
2023-06-26 15:25:31 +0200

    adding a log for entry request part 3

--- src/middlewares/endPointLogger.ts
+++ src/middlewares/endPointLogger.ts
@@ -2,18 +2,19 @@ import logger from "@/config/logger";
 
 export function logEntryRequest(req: any, res: any, next: any) {
 	logger.info(
-		"Entry Request : From [" +
+		"[API CALL] client='" +
 			req.rawHeaders[19] +
-			"] to [" +
-			req.rawHeaders[1] +
-			"] requested url = " +
-			req.url
-	);
-	logger.info(
-		"    with parameters = " +
+			"' request='" +
+			req.url +
+			"', path='" +
+			req.path +
+			"', method='" +
+			req.method +
+			"', parameters=" +
 			JSON.stringify(req.params) +
-			" and query = " +
-			JSON.stringify(req.query)
+			", query=" +
+			JSON.stringify(req.query) +
+			""
 	);
 	next();
 }
